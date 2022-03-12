71dfae1273dcac6c887a3bb4c837115e1d51eb68
JCH95 <jhorger95@gmail.com>
2022-03-12 13:17:42 -0600

    added date below header and touched up api request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,34 @@
 // Starter Code
-// let weather = {
-//     fetchWeather: function(city) {
-//         fetch("https://api.openweathermap.org/data/2.5/weather?q=washington+dc&units=imperial&appid=c369f155016b5438a2e3977424bf7b67")
-//         .then((response) => response.json())
-//         .then((data) => this.displayWeather(data));
-//     },
+// Current Date/Time
+var today = new Date();
+document.getElementById("currentDay").innerHTML = today;
+
+let weather = {
+    apiKey: "a6bb17714f8b0f6f4691c682d40d5996", 
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
 
 
-//     displayWeather: function(data) {
-//     const { name } = data;
-//     const { description } = data.weather[0];
-//     const { temp, humidity } = data.main;
-//     const { speed } = data.wind;
-//     console.log(name, temp, humidity, speed);
-//     document.querySelector(".city").innerText = name;
-//     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-//     document.querySelector(".wind").innerText = "Wind Speed: " + speed + " MPH";
-//     document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
-//     document.querySelector(".description").innerText = "Description: " + description;
-// }
-// }
+    displayWeather: function(data) {
+        const { name } = data;
+        const { description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, temp, humidity, speed);
+        document.getElementById("city-name").innerText = name;
+        document.getElementById("humidity").innerText = "Humidity: " + humidity + "%";
+        document.getElementById("wind").innerText = "Wind Speed: " + speed + " MPH";
+        document.getElementById("temp").innerText = "Temp: " + temp + "°F";
+        document.getElementById("description").innerText = "Description: " + description;
+    }
+};
 // weather.fetchWeather();
+
 var cityInput = document.getElementById("city-input");
-var searchBtn = document.getElementById("city-search");
+var searchBtn = document.getElementById("city-button");
 var cityList = document.getElementById("city-list");
 
 // Stores each city when the search button is clicked
@@ -31,12 +37,29 @@ searchBtn.addEventListener("click", (e) => {
     var city = cityInput.value;
     localStorage.setItem("city-input", JSON.stringify(city));
     listBuilder();
+    weather.fetchWeather();
+    weather.displayWeather();
 });
 
 // Pulls city from locl storage and makes a new li element
 function listBuilder() {
     var getCity = JSON.parse(localStorage.getItem("city-input"));
-    var list = document.createElement("li");
+    var list = document.createElement("button");
     list.textContent = getCity;
     cityList.appendChild(list);
-}
+};
+
+
+
+
+// API CALL TO RUN onClick OF THE SEARCH BUTTON
+// PASS USER INPUT INTO URL OF API CALL APPROPRIATELY
+// DISPLAY CITY INFO ON DASHBOARD
+// CHANGE LI ELEMENTS TO BUTTONS THAT BRING UP PREVIOUSLY SEARCHED CITIES
+// REMEMBER ICON FOR WEATHER STATUS
+// WHEN DISPLAY CITY UV INDEX, DISPLAY IN COLOR THAT MATCHES NUMBER (FAVORABLE = GREEN, MODERATE = YELLOW, SEVERE = RED) GOOGLE RANGES OF UV INDEX
+// WHEN USER CLICKS CITY IN SAVE AREA, PASS CITY INTO API CALL AND DISPLAY INFO FROM THAT CITY AGAIN
+
+// HINT
+// ABSTRACT API CALL TO A FUNCTION WITH A CITY AS THE PARAMETER TO BE PASSED INTO THE URL
+// Make callback function into named function and use it when re-searching the city
